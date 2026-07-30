66219d8029ce2af25647e68aabfee36545a81111
thiyagar987 <32755096+thiyagar987@users.noreply.github.com>
2026-07-29 20:45:49 -0400

    Added the create directory for output path

--- app/services/tts_service.py
+++ app/services/tts_service.py
@@ -1,3 +1,5 @@
+import os
+
 from gtts import gTTS
 from app.core.config import AUDIO_OUTPUT_PATH
 
@@ -7,6 +9,7 @@ def text_to_speech(text: str, language_code: str, output_path: str = AUDIO_OUTPU
     if not text.strip():
         raise ValueError("Text for speech synthesis cannot be empty.")
 
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     tts = gTTS(text=text, lang=language_code, slow=False)
     tts.save(output_path)
     return output_path
